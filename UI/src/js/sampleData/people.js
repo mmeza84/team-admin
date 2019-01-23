52fb4a2ca9d1b3940ff282c56ed71125afe25756
Matt <mmeza84@gmail.com>
2019-01-22 21:53:32 -0600

    Adding people data for rendering a team

---
+++ UI/src/js/sampleData/people.js
@@ -0,0 +1,16 @@
+export default {
+	{
+		"name": "John Player",
+		"email": "john.player@gmail.com",
+		"phone": "123-456-7890",
+		"isPlayer": false
+	},{
+		"name": "Jane Player",
+		"email": "jane.player@gmail.com",
+		"phone": "987-654-3210",
+		"isPlayer": false
+	}, {
+		"name": "Sally Player",
+		"isPlayer": true
+	}
+};
